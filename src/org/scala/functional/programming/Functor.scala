b99688583bc568fd1be2485e210fa92cca73fcfc
lambda-technica <lambda.technica@gmail.com>
2017-10-17 11:02:10 +0200

    Initial commit

---
+++ src/org/scala/functional/programming/Functor.scala
@@ -0,0 +1,13 @@
+package org.scala.functional.programming
+
+import scala.language.higherKinds
+
+trait Functor[F[_]] {
+  def map[A, B](fa: F[A])(f: A => B): F[B]
+}
+
+object Functor extends App{
+
+  print("Begin")
+
+}
